dec598e1e279f2f943ec4eb06b246c5d4224a886
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-17 03:50:18 +0400

    Adjust image height class in Header component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -26,7 +26,7 @@ export default function Header({ mobileMenuOpen, setMobileMenuOpen }: HeaderProp
             <img 
               src="/Final.png" 
               alt="Final.png" 
-              className="h-17 w-auto"
+              className="h-16 w-auto"
             />
           </a>
           
