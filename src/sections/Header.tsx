b27cdc1db2d83c0ccf4e2b52693a2370ee1f18eb
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-17 04:39:40 +0400

    Change header image height from 25 to 15

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -26,7 +26,7 @@ export default function Header({ mobileMenuOpen, setMobileMenuOpen }: HeaderProp
             <img 
              src="/Final.png" 
              alt="Final.png" 
-             className="h-25"
+             className="h-15"
           />
           </a>
           
