6c4c7f3540d9a366e269331b361c873a25a3469c
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-17 04:41:38 +0400

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -26,7 +26,7 @@ export default function Header({ mobileMenuOpen, setMobileMenuOpen }: HeaderProp
             <img 
              src="/Final.png" 
              alt="Final.png" 
-             className="h-15"
+             className="h-12"
           />
           </a>
           
