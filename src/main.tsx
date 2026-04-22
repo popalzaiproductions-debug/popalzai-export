5665f98d5db2de2d1d6b80d74cbf4d0bc69e9267
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-22 21:51:50 +0400

    Fix formatting of createRoot call in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,4 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).
+createRoot(document.getElementById('root')!).render(
