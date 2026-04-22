915f03636aef108827b1a5ff9c628b16e4807594
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-22 22:43:37 +0400

    Clean up Home component by removing sections
    Removed unused section imports and components from Home.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,4 @@
 import Hero from '../sections/Hero'
-import About from '../sections/About'
-import Services from '../sections/Services'
-import Process from '../sections/Process'
-import Work from '../sections/Work'
-import FAQ from '../sections/FAQ'
-import Contact from '../sections/Contact'
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <About />
-      <Services />
-      <Process />
-      <Work />
-      <FAQ />
-      <Contact />
-    </>
-  )
-}
