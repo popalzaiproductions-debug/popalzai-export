a423f8cc0258f449882a9439f6ea2f0b6c7b0aea
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-22 22:55:04 +0400

    Refactor Home component to include state management

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
-import Hero from '../sections/Hero'
+import { useState } from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Services from './sections/Services'
+import Process from './sections/Process'
+import Work from './sections/Work'
+import FAQ from './sections/FAQ'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+import './App.css'
 
-export default function Home() { return (
+function App() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
