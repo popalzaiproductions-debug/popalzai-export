3a3f2a81e2de6631c9eb9ce11f725ee804447a91
popalzaiproductions-debug <popalzaiproductions@gmail.com>
2026-04-22 22:57:10 +0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import Header from './sections/Header'
-import Hero from './sections/Hero'
-import About from './sections/About'
-import Services from './sections/Services'
-import Process from './sections/Process'
-import Work from './sections/Work'
-import FAQ from './sections/FAQ'
-import Contact from './sections/Contact'
-import Footer from './sections/Footer'
-import './App.css'
+import Hero from '../sections/Hero'
 
-function App() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
+export default function Home() {
   return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
